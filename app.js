823cc6d6e2b53a89f16e8663a86ebf97164dd7d5
Pineapple Season <emsthai21@gmail.com>
2020-11-21 20:15:25 -0700

    working on updating template function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const [name, github] = profileDataArgs;
 
@@ -154,5 +154,11 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage();
+
+        // fs.writeFile('./index.html', pageHTML, err => {
+        //   if (err) throw new Error(err);
+
+        //   console.log('Page created! Check out index.html in this directory to see it!');
+        // });
     });
